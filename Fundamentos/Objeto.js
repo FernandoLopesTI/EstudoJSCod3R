304882454ceb1e5128885d17e3639bc7eaedb588
Fernando Lopes <nandolopis@gmail.com>
2018-12-12 14:01:42 -0200

    Aula Objetos

---
+++ Fundamentos/Objeto.js
@@ -0,0 +1,23 @@
+//criação do objeto 
+
+/**
+ * Criação de objetos de formar dinâmica 
+ */
+const prod1 = {} 
+console.log(typeof prod1);
+
+prod1.nome = "Celular"; 
+prod1.preco = 2000.90;
+console.log(prod1);
+
+/**
+ * Criação de objetos de forma literal 
+ */
+
+const  prod2 = {
+    nome: "Celular 2",
+    preco: 1500.99,
+
+}
+
+console.log(prod2);
