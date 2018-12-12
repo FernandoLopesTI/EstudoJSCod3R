ae434e769b73a08a38ce74219072ef653a278660
Fernando Lopes <nandolopis@gmail.com>
2018-12-12 17:49:53 -0200

    Aula Notação ponto

---
+++ Fundamentos/NotacaoPonto.js
@@ -0,0 +1,31 @@
+/**
+ * Anotação ponto é a anotaçao que usamos para acessar um metodo de um obj e função
+ * Alguns exemplo de anotaçao pronto 
+ * 
+ */
+
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+
+obj1.nome = 'bola'
+console.log(obj1.nome)
+obj1['nome'] = 'bola2'
+console.log(obj1.nome)
+
+function Obj(nome) {
+    this.nome = nome
+    this.exec = function() {
+        console.log('Exec ...')
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+
+console.log(obj2.nome)
+console.log(obj3.nome)
+
+obj3.exec();
+
+
--- Fundamentos/math.js
+++ Fundamentos/math.js
@@ -8,5 +8,9 @@
 const raio = 5.6
 const area =  Math.PI * Math.pow(raio, 2);
 
+//arrendonda
+console.log(Math.ceil(6.1))
+console.log(Math.floor(6.1))
+
 console.log(area);
 console.log(typeof Math);
