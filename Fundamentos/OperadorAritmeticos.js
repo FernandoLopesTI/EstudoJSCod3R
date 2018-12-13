e26dabf482ce2a6df315af75895c7270748d9a87
Fernando Lopes <nandolopis@gmail.com>
2018-12-13 10:06:53 -0200

    Aula Operadores Aritméricos

---
+++ Fundamentos/OperadorAritmeticos.js
@@ -0,0 +1,29 @@
+/**
+ * Operadores Aritméticos trabalha de forma binária, ou seja, os operadores agem em pares 
+ * 
+ */
+
+const [numero1, numero2, numero3, numero4] = [3, 5, 1, 15]
+
+const soma = numero1 + numero2 + numero3 + numero4
+console.log(soma)
+
+const subtracao = numero4 - numero2
+console.log(subtracao)
+
+const multiplicacao = numero1 * numero2
+console.log(multiplicacao)
+
+const divisao = numero4 / numero1
+console.log(divisao)
+
+const modulo = numero1 % 2
+console.log(modulo)
+
+
+
+
+
+
+
+
