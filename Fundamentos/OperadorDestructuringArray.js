581b92cb334f0950ad96b38a028ea31c5c5e599a
Fernando Lopes <nandolopis@gmail.com>
2018-12-13 09:31:46 -0200

    Aula Operadores Destructuring com Array

--- Fundamentos/OperadorDestructuringArray.js
+++ Fundamentos/OperadorDestructuringArray.js
@@ -3,7 +3,7 @@
 const [a] = [10]
 console.log(a);
 
-
+//const [a] <-(operador de desestruturação)  = [10] <-(array)
 
 //podemos desistruturar varios atributos do array
 const [n1,,n3,,n5,n6 = 1] = [10,7,9,8]
