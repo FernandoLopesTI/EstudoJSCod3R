dfcc44879b5fa514744613f8c19ce8d7e7ee849d
Fernando Lopes <nandolopis@gmail.com>
2018-12-13 13:58:35 -0200

    Aula Extrutura if e else if

---
+++ ExtruturaDeControle/ifElseIf.js
@@ -0,0 +1,25 @@
+Number.prototype.entre = function (inicio , fim){
+    return this >= inicio && this <= fim
+}
+
+const imprimirResultado =  function (nota){
+    if(nota.entre(9, 10)){
+        console.log('Quadro de Honra!')
+    } else if(nota.entre(7,8.99)){
+        console.log('Aprovado')
+    } else if (nota.entre(4, 6.99)){
+        console.log('Recuperação')
+    } else if (nota.entre(0, 3.99)) {
+        console.log('Reprovado')
+    } else {
+        console.log('Nota inválida')
+    }
+}
+
+imprimirResultado(9.8)
+imprimirResultado(7.1)
+imprimirResultado(6.4)
+imprimirResultado(4.6)
+imprimirResultado(3.1)
+imprimirResultado(11.1)
+imprimirResultado(-2.1)
