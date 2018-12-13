f442ec519e1dee1abcb3fb9441b46e44f5e336f8
Fernando Lopes <nandolopis@gmail.com>
2018-12-13 17:00:46 -0200

    Aula Função this pode variar

---
+++ Função/ThisPodeVariar.js
@@ -0,0 +1,7 @@
+/**
+ * Dentro do javaScript o This pode variar dentro de uma função que foi declarada como a palavra reservada function
+ * dependendo de onde essa função for chamada.
+ * No browser como por exemplo quando se criarmos uma função que compare o this com o window(que é o objeto global do browser) 
+ * ele vai retornar true mais se atribuirmos essa mesma função para o clique do mouse na pagina ele vai retorna false pq o conceito de this mudou de local
+ * 
+ */
