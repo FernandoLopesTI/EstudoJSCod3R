4cea0d2b18341350eb86744da01e84fccf95ae29
Fernando Lopes <nandolopis@gmail.com>
2018-12-14 10:16:11 -0200

    Aula Função Anônima

---
+++ Função/FuncaoAnonima.js
@@ -0,0 +1,33 @@
+const soma = function (x,y){
+    return x + y
+}
+
+const imprimirResultado = function(a , b , operacao = soma){
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3, 4)
+imprimirResultado(3, 4,  soma)
+imprimirResultado(3, 4, function(x, y){
+    return x - y
+})
+imprimirResultado(3, 4, (x, y) => x * y )
+
+
+const pessoa = {
+    falar: function (){
+        console.log("Falar ")
+    }
+}
+
+pessoa.falar()
+
+//es2015
+
+const pessoa2 = {
+    falar() {
+        console.log("Falar ")
+    }
+}
+
+pessoa2.falar()
