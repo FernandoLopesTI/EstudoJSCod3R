285f26a85de2151025c04fa3fbb4254dfc558c44
Fernando Lopes <nandolopis@gmail.com>
2018-12-14 14:22:21 -0200

    Aula Função Autoinvocada

---
+++ Função/FuncaoAutoInvocadaIIFE.js
@@ -0,0 +1,10 @@
+/**
+ * IIFE (Immediately Invoked Function Expression) é uma função auto infocada 
+ */
+
+ //sintaxe de uma IIFE
+(function(){
+    console.log('Será executar na hora!')
+    console.log('Foge do escopo mais abrangente! (global)')
+})()
+
