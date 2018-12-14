3480acb2541a8cc35fce7b9b52d72090709d68e4
Fernando Lopes <nandolopis@gmail.com>
2018-12-14 15:31:26 -0200

    Aula OO

---
+++ Objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+
+ // Orientada a Objeto
+ objeto = {
+     valor1,
+     valor2,
+     valor3,
+     processamento() {
+         // ...
+     }
+ }
+
+ objeto.processamento() // Foco passou a ser o objeto
+
+ // Principios importantes:
+ // 1. abstracao. é pega um obj do mundo real e tenta abstrair ele para dentro do sistema 
+ // 2. encapsulamento.  
+ // 3. herança (prototype)
+ // 4. polimorfismo
